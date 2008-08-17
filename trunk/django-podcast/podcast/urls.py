cf6f2271abff8cf61badf866d9d53ee3ba8f67f1
richardcornish <richardcornish@7eeeb57b-9f54-0410-948d-eb906a2a6a24>
2008-08-17 04:09:57 +0000

    Removed redundant top-level directory
    git-svn-id: http://django-podcast.googlecode.com/svn@3 7eeeb57b-9f54-0410-948d-eb906a2a6a24

--- trunk/django-podcast/podcast/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('podcast.views',
-    # Podcast feed URL
-    (r'^feedburner/$', 'episode_feed'),
-
-    # Google Video sitemap
-    (r'^sitemap.xml$', 'episode_sitemap'),
-)
