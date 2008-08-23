fcbee5dd8068f096ecc62eb4c0bd88bb17487c06
jeff.triplett <jeff.triplett@7eeeb57b-9f54-0410-948d-eb906a2a6a24>
2008-08-23 04:19:10 +0000

    Added support for episode list and detail pages.
    git-svn-id: http://django-podcast.googlecode.com/svn@14 7eeeb57b-9f54-0410-948d-eb906a2a6a24

--- trunk/podcast/urls.py
+++ trunk/podcast/urls.py
@@ -2,9 +2,15 @@ from django.conf.urls.defaults import *
 
 
 urlpatterns = patterns('podcast.views',
-    # Podcast feed URL
-    (r'^feedburner/$', 'episode_feed'),
+    # Episode list of one show
+    (r'^(?P<slug>[-\w]+)/$', 'episode_list'),
 
-    # Google Video sitemap
-    (r'^sitemap.xml$', 'episode_sitemap'),
+    # Episode detail of one show
+    (r'^(?P<show_slug>[-\w]+)/(?P<episode_slug>[-\w]+)/$', 'episode_detail'),
+
+    # Episode feed of one show
+    (r'^(?P<slug>[-\w]+)/feedburner/$', 'episode_feed'),
+
+    # Episode sitemap list of one show
+    (r'^(?P<slug>[-\w]+)/sitemap.xml$', 'episode_sitemap'),
 )
--- trunk/podcast/views.py
+++ trunk/podcast/views.py
@@ -1,6 +1,23 @@
 from django.views.generic.list_detail import object_list
 from django.views.generic.list_detail import object_detail
-from podcast.models import *
+from podcast.models import Episode
+
+
+def episode_detail(request, show_slug, episode_slug):
+    """
+    Episode detail
+
+    Template:  ``podcast/episode_detail.html``
+    Context:
+        object_detail
+            Detail of episode.
+    """
+    return object_detail(
+        request,
+        queryset=Episode.objects.published().filter(show__slug__exact=show_slug),
+        slug=episode_slug,
+        slug_field='slug',
+        template_name='podcast/episode_detail.html')
 
 
 def episode_feed(request):
@@ -17,6 +34,21 @@ def episode_feed(request):
         mimetype='application/rss+xml',
         queryset=Episode.objects.published(),
         template_name='podcast/episode_feed.html')
+
+
+def episode_list(request, slug):
+    """
+    Episode list
+
+    Template:  ``podcast/episode_list.html``
+    Context:
+        object_list
+            List of episodes.
+    """
+    return object_list(
+        request,
+        queryset=Episode.objects.published().filter(show__slug__exact=slug),
+        template_name='podcast/episode_list.html')
 
 
 def episode_sitemap(request):
