5fa1a4af8a318e04556c17eea404239b695c0b2d
richardcornish <richardcornish@7eeeb57b-9f54-0410-948d-eb906a2a6a24>
2008-10-01 07:08:36 +0000

    Created url names, moved image markup around in show list template, and removed embarrassing defaults in atom template. I'll fix those templates soon. I promise.
    git-svn-id: http://django-podcast.googlecode.com/svn@40 7eeeb57b-9f54-0410-948d-eb906a2a6a24

--- trunk/podcast/urls.py
+++ trunk/podcast/urls.py
@@ -8,20 +8,20 @@ urlpatterns = patterns('podcast.views',
     (r'^(?P<slug>[-\w]+)/$', 'episode_list'),
 
     # Episode list feed by show (RSS 2.0 and iTunes)
-    url(r'^(?P<slug>[-\w]+)/feed/$', 'show_list_feed', name='show_list_feed'),
+    url(r'^(?P<slug>[-\w]+)/feed/$', 'show_list_feed', name='podcast-feed'),
 
     # Episode list feed by show (Atom)
-    (r'^(?P<slug>[-\w]+)/atom/$', 'show_list_feed'),
+    url(r'^(?P<slug>[-\w]+)/atom/$', 'show_list_feed', name='podcast-atom'),
     
     # Episode list feed by show (Media RSS)
-    (r'^(?P<slug>[-\w]+)/media/$', 'show_list_feed'),
+    url(r'^(?P<slug>[-\w]+)/media/$', 'show_list_feed', name='podcast-media'),
 
     # Episode list feed by show (Photocast)
-    (r'^(?P<slug>[-\w]+)/photocast/$', 'show_list_feed'),
+    url(r'^(?P<slug>[-\w]+)/photocast/$', 'show_list_feed', name='podcast-photo'),
 
     # Episode detail of one show
     (r'^(?P<show_slug>[-\w]+)/(?P<episode_slug>[-\w]+)/$', 'episode_detail'),
 
     # Episode sitemap list of one show
-    (r'^(?P<slug>[-\w]+)/sitemap.xml$', 'episode_sitemap'),
+    url(r'^(?P<slug>[-\w]+)/sitemap.xml$', 'episode_sitemap', name='podcast-sitemap'),
 )
