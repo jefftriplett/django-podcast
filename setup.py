177854e67cc1e4ed6814808d473ae612cd906b68
Bram Duvigneau <bramd@bruad.bramd.nl>
2010-02-14 09:19:30 +0800

    Added simple setup.py script

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-podcast',
+    version='0.1dev',
+    description='Django podcast app',
+    packages=find_packages('.'),
+    classifiers = ['Frameword :: Django'],
+)
