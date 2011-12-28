ba7a612125715cd4a90a4e8cde629d5eab3f06bc
Mark Johnson <wmdmark@gmail.com>
2011-12-28 11:25:09 -0500

    include templates in setup

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 from setuptools import setup, find_packages
 
+template_patterns = [
+    'templates/*.html',
+    'templates/*/*.html',
+]
+
+packages = find_packages('.')
+
 setup(
     name='django-podcast',
     version='0.1dev',
     description='Django podcast app',
-    packages=find_packages('.'),
+    packages=packages,
+    package_data=dict((package_name, template_patterns) for package_name in packages),
     classifiers = ['Frameword :: Django'],
 )
